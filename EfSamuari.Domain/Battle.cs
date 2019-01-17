445bcf4cd2be3575e15b4018ffe543d6600efd0b
oscarcarlsson93 <oscargbcarlsson@gmail.com>
2019-01-17 14:42:42 +0100

    Lagt till en många-till-många relation; SamuraiBattle

---
+++ EfSamuari.Domain/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfSamurai.Domain
+{
+   public class Battle
+    {
+        public int Id { get; set; }
+        public string BattleName { get; set; }
+        public string Description { get; set; }
+        public bool Brutal { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+    }
+}
---
+++ EfSamuari.Domain/SamuraiBattle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfSamurai.Domain
+{
+    public class SamuraiBattle
+    {
+        public int SamuraiId { get; set; }
+        public Samurai Samurai { get; set; }
+
+        public int BattleId { get; set; }
+        public Battle Battle { get; set; }
+
+    }
+}
